2a8e2afd46f056c47260c0af6946dcd2e2005db2
qclass <qclass@tutanota.com>
2019-02-27 09:01:59 -0800

    JWT Auth

---
+++ api/run.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_pymongo import PyMongo
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'my-secret-string'
+app.config["MONGO_URI"] = "mongodb://cybexp_user:CybExP_777@134.197.21.231:27017/cache_db?authSource=admin"
+app.config["MONGO_DBNAME"] = "cache_db"
+
+mongo = PyMongo(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+jwt = JWTManager(app)
+
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+import views, models, resources
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
+api.add_resource(views.Event,'/api/v1.0/event')
---
+++ api/views.py
@@ -0,0 +1,31 @@
+from flask_restful import Resource, reqparse
+import werkzeug, pytz, pymongo
+from datetime import datetime
+from flask_jwt_extended import jwt_required
+
+# Post Events to API
+event_parser = reqparse.RequestParser()
+event_parser.add_argument('orgid', type=str, required=True)
+event_parser.add_argument('file', type=werkzeug.datastructures.FileStorage,
+                    location='files', required=True)
+event_parser.add_argument('typtag', type=str, required=True)
+event_parser.add_argument('timezone', type=str, required=True)
+
+class Event(Resource):
+    decorators=[]
+    @jwt_required  
+    def post(self):
+        request = event_parser.parse_args()
+        f = request['file']
+        
+        info = {}
+        info['datetime'] = datetime.now(pytz.utc).isoformat()
+        info['orgid'] = request['orgid']
+        try: info['fid'] = mongo.save_file(f.filename, f)
+        except pymongo.errors.ServerSelectionTimeoutError: return ({'message': 'Database down'}, 500)
+        info['processed'] = False
+        info['typtag'] = request['typtag']
+        info['timezone'] = request['timezone']
+
+        i = mongo.db.file_entries.insert_one(info)
+        return ({'message': 'File Uploaded Succesfully'}, 201)
