169539684d0fc8c08467725e7064a7575f9f7bb5
qclass <qclass@tutanota.com>
2019-04-25 22:21:35 -0700

    API README Update

--- api/views/related.py
+++ api/views/related.py
@@ -7,16 +7,14 @@ else:
 # Load Report Database
 rcoll = coll_or_fs('report')
 
-def get_related(**kwargs):
+def get_related(obj_typ, obj_val, **kwargs):
     result_bundle = []
+
+    query = { "$and" : [ {"objects.0.type" : obj_typ} ]}
     
-    # Find matching object
-    obj_typ = kwargs['obj_typ']
-    if obj_typ in ['ipv4-addr', 'url']:
-        obj_val = kwargs['obj_val']
-        query = { "$and" : [ {"objects.0.value" : obj_val},
-                             {"objects.0.type" : obj_typ} ]}
-    else: return None   
+    # Take of special structures
+    if obj_typ == 'file': query["$and"].append({"objects.0.hashes.SHA-256" : obj_val} )
+    else: query["$and"].append({"objects.0.value" : obj_val} )
         
     obj = rcoll.find_one(query, _PROJECTION)
     if not obj: return None
@@ -46,32 +44,37 @@ def get_related(**kwargs):
 
 
 rparser = reqparse.RequestParser()
-rparser.add_argument('ipv4-addr')
-rparser.add_argument('url')
+for va in _VALID_ATT:
+    rparser.add_argument(va)
 
 class Related(Resource):
-##    decorators = []
 
     @jwt_required
-    def post(self):
+    def post(self):        
+        # Validate Request
         example = { "ipv4-addr" : "104.168.138.60" }
+        status_code, r = 200, {"example" : example, "documentation" : _DOCUMENTATION}
+
         req = rparser.parse_args()
-
-        valid_obj_typ = ['ipv4-addr', 'url']
         req_keys = req.keys()
-
+        
         count = 0
-        for ot in valid_obj_typ:
+        for ot in _VALID_ATT:
             ov = req[ot]
             if ov:
                 obj_typ = ot
                 obj_val = ov 
                 count += 1
-        if count < 1: return ({'message': 'Input valid attribute object, check spelling', 'example':example}, 400)
-        elif count > 1: return ({'message' : 'Input one attribute object at a time', 'example':example}, 400)
+        if count < 1:  message, status_code = 'Input valid attribute object, check spelling', 400
+        elif count > 1: message, status_code = 'Input one attribute object at a time', 400
 
+        if status_code != 200:
+            r['message'] = message
+            return (r, status_code)
+        
+        # Get Related
         obj_val = req[obj_typ]
-        r = get_related(obj_typ = obj_typ, obj_val = obj_val)
+        r = get_related(obj_typ, obj_val)
 
         if not r:  r = {'message': obj_typ + ' object not found: ' + obj_val}
         return (r, 200)
--- api/views/views_comm.py
+++ api/views/views_comm.py
@@ -16,4 +16,18 @@ import builtins
 builtins._PROJECTION = {"_id":0, "filters":0, "bad_data":0}
 builtins._REPORT_ORGID = "identity--7f60ac36-74dd-4c23-bc31-3226533d93d2"
 builtins._QLIM = 10000
-builtins._VALID_ATT = {'ip' : 'ipv4-addr', 'url':'url'}
+builtins._DOCUMENTATION = "https://github.com/CYBEX-P/cybexp-cs/tree/master/api"
+builtins._VALID_ATT = {'ip' : 'ipv4-addr',
+                       'ipv4' : 'ipv4-addr',
+                       'ipv4-addr' : 'ipv4-addr',
+                       'url':'url',
+                       'email' : 'email-addr',
+                       'email-addr' : 'email-addr',
+                       'domain-name' : 'domain-name',
+                       'domain' : 'domain-name',
+                       'mac-addr' : 'mac-addr',
+                       'mac' : 'mac-addr',
+                       'file' : 'file',
+                       'file-hash' : 'file',
+                       'file-sha256' : 'file',
+                       'file-hash-sha256' : 'file'}
