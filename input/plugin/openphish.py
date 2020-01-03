a838a8f64b92259675a31e887ef7d2b6b4724e20
Ignacio Astaburuaga <ignaciochg@nevada.unr.edu>
2020-01-03 11:04:38 -0800

    Add OpenPish to input module

---
+++ input/plugin/openphish.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from .common import *
+
+FEED_URL = "https://openphish.com/feed.txt"
+
+class OpenpishSource(CybexSource):
+    def fetch_and_post(self):
+
+        logging.info(f"Retrieving events from OpenPish at {FEED_URL}")
+        response = requests.get(FEED_URL)
+
+        if response.ok:
+
+            text = response.text
+            count = text.count('\n')
+            logging.info(f"Retrieved {count} records from OpenPish.")
+            self.post_event_to_cybex_api(text)
+
+        else:
+            logging.error(
+                "api.input.openpishSource.fetch -- \n\t" + response.reason
+            )
