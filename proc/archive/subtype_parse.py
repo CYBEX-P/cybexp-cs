2eed91beddaa3119e38b8a97122ceeda746aa505
Ignacio Astaburuaga <ignaciochg@nevada.unr.edu>
2020-02-06 13:37:58 -0800

    finished filter openphish filter module

--- proc/archive/subtype_parse.py
+++ proc/archive/subtype_parse.py
@@ -1,5 +1,5 @@
 import json 
-
+import time
 
 def doNothing(data):
    return data
@@ -16,7 +16,18 @@ def phishtank_api_parser(data):
    return doNothing(data)
 
 def openphish_file_feed_parser(data):
-   new_data = dict({"URLs": list(data.split())})
-   return json.dumps(new_data)
+   try:
+      #if not comminity edition, payware
+      json_object = json.loads(data) # should already be in json
+      json_object["openphish_type"] = "premium"
+      json_object["timestamp"] = time.time()
+      return json.dumps(json_object)
+
+   except ValueError: # else is community free version
+
+      new_data = dict({"URLs": list(data.split()),
+                        "timestamp": time.time(),
+                        "openphish_type": "community"})
+      return json.dumps(new_data)
 
 
